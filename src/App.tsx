e945de35b7e3e9ec000367aa3ed72cdf7ec95c7d
Aescudeiro <afonso_escudeiro@hotmail.com>
2022-01-19 21:37:16 -0100

    created 404 route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./views/Home/Home";
 import { MetaMaskInpageProvider } from "@metamask/providers";
+import { NotFound } from "./views/NotFound/NotFound";
 
 declare global {
   interface Window {
@@ -12,6 +13,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
