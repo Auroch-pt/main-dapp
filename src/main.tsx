1e11c04b42a94e081db8adb886a29b089f511fa0
Aescudeiro <afonso_escudeiro@hotmail.com>
2022-01-22 22:47:10 -0100

    added apollo client provider and connected to hasura

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,26 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: import.meta.env.HASURA_ENDPOINT,
+  }),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
