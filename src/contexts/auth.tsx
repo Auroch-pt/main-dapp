9bfa0b22f460e12da2198f1cef98f9f145cbe3bc
Aescudeiro <afonso_escudeiro@hotmail.com>
2022-01-23 00:11:10 -0100

    create user in hasura and store it in localstorage

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,8 +1,9 @@
-import { createContext, FC, useContext, useState } from "react";
-
+import { createContext, FC, useContext, useEffect, useState } from "react";
+import { useCreateUserMutation } from "../graphql/graphql";
 interface AuthContextData {
   isConnected: boolean;
   handleConnectWallet(): Promise<void>;
+  handleDisconnect(): Promise<void>;
   walletAccount: string;
 }
 
@@ -11,7 +12,9 @@ export const AuthContext = createContext<AuthContextData>(
 );
 
 export const AuthProvider: FC = ({ children }) => {
-  const [walletAccount, setAccount] = useState<string>("");
+  const [isConnected, setIsconected] = useState<boolean>(false);
+  const [walletAccount, setWalletAccount] = useState<string>("");
+  const [createUserMutation] = useCreateUserMutation();
 
   const handleConnectWallet = async () => {
     const provider = window.ethereum;
@@ -19,14 +22,46 @@ export const AuthProvider: FC = ({ children }) => {
     const accounts = await provider.request({ method: "eth_requestAccounts" });
     const account = accounts[0];
 
-    setAccount(account);
+    createUser(account);
   };
+
+  const handleDisconnect = async () => {
+    setIsconected(false);
+    setWalletAccount("");
+  };
+
+  const createUser = async (address: string) => {
+    await createUserMutation({
+      variables: {
+        address,
+      },
+    })
+      .then((res) => {
+        setWalletAccount(res.data.insertUser.address);
+        localStorage.setItem("address", res.data.insertUser.address);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  useEffect(() => {
+    const storagedAddress = localStorage.getItem("address");
+    if (storagedAddress) {
+      setWalletAccount(storagedAddress);
+    }
+  }, []);
+
+  useEffect(() => {
+    setIsconected(walletAccount ? true : false);
+  }, [walletAccount]);
 
   return (
     <AuthContext.Provider
       value={{
-        isConnected: walletAccount ? true : false,
+        isConnected,
         handleConnectWallet,
+        handleDisconnect,
         walletAccount,
       }}
     >
