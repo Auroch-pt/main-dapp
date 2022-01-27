2c6935408c34d9ffb9163ba89fa396406cd13f83
Afonso Escudeiro <afonso_escudeiro@hotmail.com>
2022-01-27 22:57:26 -0100

    created basic breadcrumbs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import useBreadcrumbs from "use-react-router-breadcrumbs";
 import { ProjectsList, Header } from "./Components";
 import { Home, Projects, Project } from "./Pages";
 
 function App() {
+    const breadcrumbs = useBreadcrumbs();
+
     return (
         <div>
             <Header />
+            {breadcrumbs.map(({ breadcrumb, match }) => (
+                <span key={match.pathname} style={{ marginLeft: "1em" }}>
+                    <Link to={match.pathname}>⁄{breadcrumb}</Link>
+                </span>
+            ))}
             <Routes>
                 <Route index element={<Home />} />
                 <Route path="projects" element={<Projects />}>
